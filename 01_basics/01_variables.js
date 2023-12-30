cd794b1d55499b594d39ac3f5f6fd001d6e4f93d
Siddharth Parashar <74417028+sidparashar2001@users.noreply.github.com>
2023-12-30 06:49:58 +0000

    01 Complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 14454;
+let accountEmail = 'sid@outlook.com';
+var accountPassword = "password";
+accountCity = "Jaipur";
+
+// accountID =2; // not allowed for const variables to update the value others like var and let we can update
+
+console.log(accountID);
