b8d8c76f11d5dfa8a7d674117161ca823598d991
sidparashar2001 <siddharthparashar2001@gmail.com>
2023-12-30 23:25:12 +0530

    Updated

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,17 @@
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+// console.log(true+)
+
+// console.log(+"")//0
+// console.log(""+)//error
+
